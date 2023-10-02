5da8f1608fdacb8f55362215d938d9a33087ecbc
noyon-podder <noyon.vai369@gmail.com>
2023-10-03 01:16:25 +0600

    404 page design

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,20 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>Page is nothing found</div>;
+  const router = useRouter();
+  setInterval(() => {
+    router.push("/dashboard");
+  }, 5000);
+  return (
+    <div className="flex items-center justify-center flex-col gap-5 h-screen w-full">
+      <h1 className="text-7xl font-extrabold text-heading">404</h1>
+      <h6 className="text-2xl font-semibold text-heading">
+        Page nothing found
+      </h6>
+    </div>
+  );
 };
 
 export default NotFoundPage;
