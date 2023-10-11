f793c50c12898e59cd7b38305e65e01550861f81
noyon-podder <noyon.vai369@gmail.com>
2023-10-11 18:04:59 +0600

    property card add this commit

--- src/app/(withLayout)/property/page.js
+++ src/app/(withLayout)/property/page.js
@@ -1,8 +1,12 @@
+import PropertyCard from "@/components/property/PropertyCard";
 import PropertyFilter from "@/components/property/PropertyFilter";
 import React from "react";
 import { BsPlus } from "react-icons/bs";
 
-const PropertyPage = () => {
+const PropertyPage = async () => {
+  const res = await fetch("http://localhost:5000/property");
+  const properties = await res.json();
+
   return (
     <div>
       {/* property heading */}
@@ -17,6 +21,12 @@ const PropertyPage = () => {
       {/* property content */}
       <div className=" px-[18px] md:px-5 py-5 bg-white dark:bg-darkWhite rounded-[15px] mt-5">
         <PropertyFilter />
+
+        <div className="grid md:grid-cols-2 grid-cols-1 lg:gap-x-[65px] gap-y-6 mt-[25px]">
+          {properties?.map((item) => (
+            <PropertyCard key={item.id} property={item} />
+          ))}
+        </div>
       </div>
     </div>
   );
