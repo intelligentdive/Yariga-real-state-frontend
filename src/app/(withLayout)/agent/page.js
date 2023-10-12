1cbdd33dca0260cee8bf99dccc83bbecb512cd92
noyon-podder <noyon.vai369@gmail.com>
2023-10-12 13:37:46 +0600

    agent page modified complete

--- src/app/(withLayout)/agent/page.js
+++ src/app/(withLayout)/agent/page.js
@@ -1,30 +1,27 @@
 import AllAgent from "@/components/agent/AllAgent";
+import { AiOutlinePlus } from "react-icons/ai";
 
+const AgentPage = async () => {
+  const res = await fetch("http://localhost:5000/all-agent");
+  const agents = await res.json();
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <h1 className="text-heading dark:text-darkHeading md:text-[25px] text-[18px] font-bold">
+          Agent List
+        </h1>
+        <button className="flex items-center gap-[5px] py-[13px] px-5 bg-main  duration-300 hover:bg-indigo-700 text-white rounded-[10px] text-sm font-medium">
+          <AiOutlinePlus className="text-white" /> Add Agent
+        </button>
+      </div>
 
-const page = async () => {
-    const res = await fetch("http://localhost:5000/all-agent");
-    const agents = await res.json();
-    return (
-        <div>
-
-            <div className="flex justify-between items-center">
-                <h1 className="text-heading dark:text-darkHeading md:text-[25px] text-[18px] font-bold">Agent List</h1>
-                <button class="flex items-center md:px-[13px] md:py-[13px] px-[10px] py-[10px] font-small tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-                    <svg class="w-3 h-3 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M10 2a1 1 0 011 1v6h6a1 1 0 110 2h-6v6a1 1 0 11-2 0v-6H3a1 1 0 110-2h6V3a1 1 0 011-1z" />
-                    </svg>
-                    <span class="mx-1">Add Agent</span>
-                </button>
-            </div>
-
-                <div className="mt-[20px]">
-                    {
-                        agents?.map(agent => <AllAgent key={agent?.id} agent={agent}></AllAgent>)
-                    }
-                </div>
-
-        </div>
-    );
+      <div className="mt-[20px]">
+        {agents?.map((agent) => (
+          <AllAgent key={agent?.id} agent={agent}></AllAgent>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default AgentPage;
--- src/components/agent/AllAgent.jsx
+++ src/components/agent/AllAgent.jsx
@@ -1,43 +1,71 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
+import {
+  BsFillBuildingsFill,
+  BsFillEnvelopeFill,
+  BsTelephone,
+} from "react-icons/bs";
+import { FaLocationDot } from "react-icons/fa6";
 
 const AllAgent = ({ agent }) => {
-    return (
-        <div className="md:flex items-start justify-between mb-[20px] bg-white md:p-[20px] dark:bg-black">
-            <div className="flex gap-[10px] w-full">
-                <Image src={agent?.image} alt="Agent image" className="w-[130px] h-[110px] md:w-[250px] md:h-[193px]" width={250}
-                    height={193} />
-                <div className="flex flex-col ml-[20px]">
-                    <div className="md:mb-[52px] text">
-                        <h2 className="text-heading dark:text-darkHeading font-semibold md:text-[22px] text-[14px] mb-[7px]">
-                            Benny Chagur
-                        </h2>
-                        <p className="text-text dark:text-darkText font-normal text-[12px] lg:text-[14px] md:mb-0 mb-[8px]">
-                            Real-Estate Agent
-                        </p>
-                    </div>
+  return (
+    <div className="md:flex items-start justify-between mb-[20px] bg-white md:p-[20px] dark:bg-darkWhite rounded-[10px]">
+      <div className="flex gap-[10px] md:gap-5 w-full">
+        <Image
+          src={agent?.image}
+          alt="Agent image"
+          className="w-[130px] h-[110px] md:w-[250px] md:h-[193px]"
+          width={250}
+          height={193}
+        />
+        <div className="flex flex-col py-2">
+          <div className="md:mb-[52px] ">
+            <h2 className="text-heading dark:text-darkHeading font-semibold md:text-[22px] text-[14px] mb-[7px]">
+              Benny Chagur
+            </h2>
+            <p className="text-text dark:text-darkText font-normal text-[12px] lg:text-[14px] md:mb-0 mb-[8px]">
+              Real-Estate Agent
+            </p>
+          </div>
 
-                    <div className="md:flex lg:flex-row flex-col items-center w-full">
-                        <div className=" lg:min-w-[400px] w-full">
-                            <h3 className="text-text dark:text-darkText font-normal text-[12px] lg:text-[14px] md:mb-0 mb-[5px] md:mb-5">{agent?.email}</h3>
-                            <h3 className="text-text dark:text-darkText font-normal text-[12px] lg:text-[14px] md:mb-0 mb-[5px]">{agent?.phone}</h3>
-                        </div>
-                        <div className="">
-                            <p className="text-text dark:text-darkText font-normal text-[12px] lg:text-[14px] md:mb-0 mb-[5px] md:mb-5">{agent?.location}</p>
-                            <h3 className="text-text dark:text-darkText font-normal text-[12px] lg:text-[14px] md:mb-0 mb-[5px]">{agent?.property_number} Properties</h3>
-                        </div>
-                    </div>
+          <div className="grid grid-cols-1 lg:grid-cols-10 w-full lg:gap-16">
+            {/* email */}
+            <div className="lg:col-span-5 ">
+              <h3 className="flex items-center gap-[10px] text-text dark:text-darkText text-xs font-normal md:text-sm">
+                <BsFillEnvelopeFill className="text-sm" />
+                {agent?.email}
+              </h3>
 
-                </div>
+              {/* phone */}
+
+              <h3 className="flex items-center gap-[10px] text-text dark:text-darkText text-xs font-normal md:text-sm mt-[5px] lg:mt-5 ">
+                <BsTelephone className="text-sm" />
+                {agent?.phone}
+              </h3>
             </div>
-            <div className="hidden md:block">
-                <BiDotsHorizontalRounded className="text-text text-xl cursor-pointer" />
-            </div>
+            <div className="lg:col-span-5 mt-1 lg:mt-0">
+              <h3 className="flex items-center gap-[10px] text-text dark:text-darkText text-xs font-normal md:text-sm">
+                <FaLocationDot className="text-sm" />
+                {agent?.location}
+              </h3>
 
+              {/* phone */}
+
+              <h3 className="flex items-center gap-[10px] text-text dark:text-darkText mt-[5px] text-xs font-normal md:text-sm lg:mt-5 ">
+                <BsFillBuildingsFill className="text-sm" />
+                {agent?.property_number} Properties
+              </h3>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="hidden md:block">
+        <BiDotsHorizontalRounded className="text-text text-xl cursor-pointer" />
+      </div>
+    </div>
+  );
 };
 
 export default AllAgent;
