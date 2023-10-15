2854e75f8cb87cf547974ccfe4a7a4f04d679f16
noyon-podder <noyon.vai369@gmail.com>
2023-10-15 13:20:02 +0600

    review page is completed

--- src/app/(withLayout)/reviews/page.js
+++ src/app/(withLayout)/reviews/page.js
@@ -3,14 +3,17 @@ import ReviewTab from "@/components/review/ReviewTab";
 
 const ReviewsPage = () => {
   return (
-    <div className="px-[10px] md:px-0">
-      <h2 className="text-lg font-semibold md:text-[26px] md:font-bold text-heading dark:text-darkHeading">
+    <div className="md:px-[10px] px-0">
+      <h2 className="text-lg font-semibold md:text-[26px] md:font-bold text-heading dark:text-darkHeading md:px-[0px] px-[10px]">
         Review List
       </h2>
       <div className="my-5 ">
         <ReviewTab />
       </div>
       <div>
+        <ReviewCard />
+        <ReviewCard />
+        <ReviewCard />
         <ReviewCard />
       </div>
     </div>
--- src/components/review/ReviewCard.jsx
+++ src/components/review/ReviewCard.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import image1 from "../../assets/review/1.png";
 import Link from "next/link";
+import { AiFillStar } from "react-icons/ai";
 
 const ReviewCard = () => {
   return (
-    <div className="p-5 bg-white dark:bg-darkWhite rounded-[15px]">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-[8px]">
+    <div className="p-5 bg-white dark:bg-darkWhite md:rounded-[15px] md:mb-5">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-[16px]">
         <div className="lg:col-span-3 ">
           <div className="flex gap-[15px]">
             <div>
@@ -56,9 +57,28 @@ const ReviewCard = () => {
             </div>
           </div>
         </div>
-        <div className="lg:col-span-3 ">c</div>
-        <div></div>
-        <div></div>
+        <div className="lg:col-span-3 ">
+          <div className="flex items-center gap-[10px]">
+            <h2 className="text-heading dark:text-darkHeading text-[22px] font-semibold">
+              5.0
+            </h2>
+            <div className="flex items-center gap-[3px]">
+              <AiFillStar className="text-[#F2C94C] text-xl" />
+              <AiFillStar className="text-[#F2C94C] text-xl" />
+              <AiFillStar className="text-[#F2C94C] text-xl" />
+              <AiFillStar className="text-[#F2C94C] text-xl" />
+              <AiFillStar className="text-[#F2C94C] text-xl" />
+            </div>
+          </div>
+          <div className="flex items-center gap-2 mt-[15px]">
+            <button className="py-[8px] px-[16px] rounded-[18px] border bg-transparent border-[#EB5757] text-[#EB5757] text-sm font-normal duration-300 hover:bg-[#EB5757] hover:text-white">
+              Reject
+            </button>
+            <button className="py-[8px] px-[16px] rounded-[18px] border bg-[#2ED480] border-[#2ED480] text-white text-sm font-normal duration-300 hover:bg-transparent hover:text-[#2ED480]">
+              Approve
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
